72c08b4c8915463e7f180d5dfd54020d0a7693c2
achtuber <achtuber@gmail.com>
2023-03-05 16:24:58 +0530

    sd

--- conn.js
+++ conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/bookBackend', {
+mongoose.connect('mongodb+srv://adityav:anaya@cluster0.r6f8ysb.mongodb.net/?retryWrites=true&w=majority', {
     useUnifiedTopology: true
 }).then(() => {
     console.log("Connection is set.")
