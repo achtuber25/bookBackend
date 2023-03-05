c0b482d69b0bca674173f24dd3c9651de494df41
achtuber <achtuber@gmail.com>
2023-03-05 18:37:11 +0530

    sd

--- index.js
+++ index.js
@@ -56,10 +56,14 @@ io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
     socket.on("join_room", (data) => {
+        console.log(`join room: ${data}`);
+
         socket.join(data);
     });
 
     socket.on("send_message", (data) => {
+        console.log(`send_message: ${data}`);
+
         socket.to(data.room).emit("receive_message", data);
     });
 });
